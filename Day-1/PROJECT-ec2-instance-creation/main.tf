f013288de28e09e9e59e00441543a75690405f48
Sri Nidhi Vetcha <121305803+Srinidhi1368@users.noreply.github.com>
2024-06-11 14:38:57 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
+# here I am creating an AWS-instance with the following 
+
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+      ami           = " "  # Specify an appropriate AMI ID
+    instance_type = "t2.micro" # specify what instance type you want to create a instance
+    subnet_id = "" # specify subnet-id      -> if not create one add that id
+    key_name = "" # specify key pair name
+    tags = {   # tags are optional  without the tags also it will create the instance 
+      Name = "" # specify the name of the instance
+    }
 }
